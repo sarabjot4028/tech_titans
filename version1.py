ce049963fbe3b5102cdc2813fbc70112a1f2b36f
manwinder28 <130957942+manwinder28@users.noreply.github.com>
2024-04-22 20:28:48 +0530

    Error fixed and code pushed

--- version1.py
+++ version1.py
@@ -86,42 +86,47 @@ def word_relationship(word):
     return relationships.get(word, 'something interesting')
 
 def hangman():
-    word_to_guess = choose_word()
-    guessed_letters = []
-    attempts = 6
-    clue_given = False
+    while True:
+        word_to_guess = choose_word()
+        guessed_letters = []
+        attempts = 6
+        clue_given = False
 
-    print("Welcome to Hangman!")
-    print("Try to guess the word.")
-    print("You have 6 attempts to guess the word correctly.")
-    print("Here's a clue about the word: " + word_relationship(word_to_guess))
+        print("Welcome to Hangman!")
+        print("Try to guess the word.")
+        print("You have 6 attempts to guess the word correctly.")
+        print("Here's a clue about the word: " + word_relationship(word_to_guess))
 
-    while attempts > 0:
-        print(draw_hangman(6 - attempts))
-        print(display_word(word_to_guess, guessed_letters))
-        guess = input("Guess a letter: ").lower()
+        while attempts > 0:
+            print(draw_hangman(6 - attempts))
+            print(display_word(word_to_guess, guessed_letters))
+            guess = input("Guess a letter: ").lower()
 
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single letter.")
-            continue
+            if len(guess) != 1 or not guess.isalpha():
+                print("Please enter a single letter.")
+                continue
 
-        if guess in guessed_letters:
-            print("You already guessed that letter.")
-            continue
+            if guess in guessed_letters:
+                print("You already guessed that letter.")
+                continue
 
-        guessed_letters.append(guess)
+            guessed_letters.append(guess)
 
-        if guess not in word_to_guess:
-            attempts -= 1
-            print(f"Incorrect guess! You have {attempts} attempts left.")
-            if attempts == 0:
-                print("Sorry, you lost. The word was:", word_to_guess)
+            if guess not in word_to_guess:
+                attempts -= 1
+                print(f"Incorrect guess! You have {attempts} attempts left.")
+                if attempts == 0:
+                    print("Sorry, you lost. The word was:", word_to_guess)
+                    break
+            else:
+                print("Correct guess!")
+
+            if all(letter in guessed_letters for letter in word_to_guess):
+                print("Congratulations, you won!")
                 break
-        else:
-            print("Correct guess!")
-
-        if all(letter in guessed_letters for letter in word_to_guess):
-            print("Congratulations, you won!")
+        
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
             break
 
 if _name_ == "_main_":
