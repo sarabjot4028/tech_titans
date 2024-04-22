1109acdd744530dee6032bbafd8807604f2dbdbc
manwinder28 <130957942+manwinder28@users.noreply.github.com>
2024-04-22 20:10:27 +0530

    Error fixed and code uploaded

--- version1.py
+++ version1.py
@@ -13,34 +13,115 @@ def display_word(word, guessed_letters):
             displayed_word += "_"
     return displayed_word
 
+def draw_hangman(attempts):
+    stages = [
+        """
+           _______
+          |/      |
+          |     
+          |     
+          |     
+          |   
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |     
+          |     
+          |   
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |       |
+          |       |
+          |   
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|
+          |       |
+          |   
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |   
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |      /
+         |""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |      / \\
+         |"""
+    ]
+    return stages[attempts]
+
+def word_relationship(word):
+    relationships = {
+        'python': 'a programming language',
+        'hangman': 'a guessing game',
+        'game': 'a form of entertainment',
+        'programming': 'the act of writing code',
+        'computer': 'a machine that performs computations',
+        'science': 'the study of the natural world'
+    }
+    return relationships.get(word, 'something interesting')
+
 def hangman():
-    print("Welcome to Hangman!")
     word_to_guess = choose_word()
     guessed_letters = []
     attempts = 6
+    clue_given = False
 
-    while True:
-        print("\n" + display_word(word_to_guess, guessed_letters))
-        print("Attempts left:", attempts)
+    print("Welcome to Hangman!")
+    print("Try to guess the word.")
+    print("You have 6 attempts to guess the word correctly.")
+    print("Here's a clue about the word: " + word_relationship(word_to_guess))
+
+    while attempts > 0:
+        print(draw_hangman(6 - attempts))
+        print(display_word(word_to_guess, guessed_letters))
         guess = input("Guess a letter: ").lower()
 
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
         if guess in guessed_letters:
-            print("You've already guessed that letter!")
+            print("You already guessed that letter.")
             continue
 
         guessed_letters.append(guess)
 
         if guess not in word_to_guess:
             attempts -= 1
-            print("Incorrect guess!")
+            print(f"Incorrect guess! You have {attempts} attempts left.")
             if attempts == 0:
-                print("Sorry, you're out of attempts! The word was:", word_to_guess)
+                print("Sorry, you lost. The word was:", word_to_guess)
                 break
         else:
             print("Correct guess!")
 
         if all(letter in guessed_letters for letter in word_to_guess):
-            print("Congratulations, you've guessed the word:", word_to_guess)
+            print("Congratulations, you won!")
             break
 
 if _name_ == "_main_":
